14d89339733b7c812d76d172da9d4e386a778187
Enmar123 <js_enmar1@hotmail.com>
2020-06-18 21:55:04 -0400

    add cv2 visualization

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ Created on Fri May 29 14:21:24 2020
 """
 
 import numpy as np
-import queue 
+import queue
+import cv2 as cv
 
 class Node:
     def __init__(self, position):
@@ -33,22 +34,24 @@ class Mesh:
         
 class MeshMaker:
     def __init__(self, image):
-        self.image = image
+        self.image = cv.resize(np.array(image,dtype="uint8"), (250,250),interpolation = cv.INTER_NEAREST)
         self.mesh = Mesh()
         self.q = queue.Queue()
         
+        self.pixel_blue = np.array([255,0,0],dtype="uint8")
+        self.pixel_red = np.array([0,0,255],dtype="uint8")
+        
     def solveNextNode(self):
-        if not self.q.empty():
-            current_node = self.q.get()
-            u,v = current_node.getPosition()
-            self.image[u][v] = [255,0,0] #set node to "red" color to know it is complete
-            surroundings = getSurroundings(current_node.getPosition(), self.image)
-            for position in surroundings:
-                new_node = Node(position)
-                new_node.addConnection(current_node)
-                current_node.addConnection(new_node)
-                self.q.put(new_node)
-            self.mesh.addNode(current_node)
+        current_node = self.q.get()
+        current_position = current_node.getPosition()
+        self.image[current_position[0],current_position[1]] = self.pixel_blue #set node to "blue" color to know it is complete
+        surroundings = self.getSurroundings(current_position)
+        for position in surroundings:
+            new_node = Node(position)
+            new_node.addConnection(current_node)
+            current_node.addConnection(new_node)
+            self.q.put(new_node)
+        self.mesh.addNode(current_node)
             
         
     def makeMesh(self):
@@ -57,50 +60,36 @@ class MeshMaker:
         self.q.put(first_node)
         while not self.q.empty():
             self.solveNextNode()
-            print("solving node")
-        print("finished mesh")
-
-def matrix_to_mesh():
-    # Transforms an image into an 8-mesh representation
-    mesh = Mesh()
-    image = [[[255,255,255],[255,255,255],[255,255,255]],
-             [[255,255,255],   [0,0,0],   [255,255,255]],
-             [[255,255,255],[255,255,255],[255,255,255]]]
-    
-    position = findStartPosition(image)
-    first_node = Node(position)
-    q = queue.Queue(first_node)
-    if not q.empty():
-        current_node = q.get()
-        surroundings = getSurroundings(current_node.getPosition(), image)
-        for position in surroundings:
-            new_node = Node(position)
-            new_node.addConnection(current_node)
-            current_node.addConnection(new_node)
-            q.put(new_node)
-        mesh.addNode(current_node)
-        u,v = current_node.getPosition()
-        image[u][v] = [255,0,0] #set node to "red" color to know it is complete
+            self.showImage()
+            #print("solving node")
+        #print("finished mesh")
+        cv.destroyAllWindows()
         
-    
-    
-    
-    mesh.append(Node([u,v]))
+    def showImage(self):
+        cv.imshow("Image", self.image)
+        cv.waitKey(1)
 
 
-def getSurroundings(uv, image):
-    # Retuns a list of viable surrounding locations
-    surroundings = []
-    u,v = uv
-    height = len(image)
-    width = len(image[0])
-    for i in range(3):
-        un = (u - 1) + i
-        for j in range(3):
-            vn = (v - 1) + j
-            if (0 <= un < height) and (0 <= vn < width) and ([un,vn] != [u,v]) and isPixelWhite(image[un][vn]):
-                surroundings.append([un,vn])
-    return surroundings 
+    def getSurroundings(self, uv):
+        # Retuns a list of viable surrounding locations
+        surroundings = []
+        u,v = uv
+        height = len(self.image)
+        width = len(self.image[0])
+        for i in range(3):
+            un = (u - 1) + i
+            if (0 <= un < height):
+                for j in range(3):
+                    vn = (v - 1) + j    
+                    if (0 <= vn < width):
+                        if ([un,vn] != [u,v]) and isPixelWhite(self.image[un][vn]):
+                            surroundings.append([un,vn])
+                            self.image[un,vn] = self.pixel_red
+                    else:
+                        pass
+            else:
+                pass
+        return surroundings 
     
 def findStartPosition(image):
     # Returns the first position in an image that can be used
@@ -126,4 +115,12 @@ def isPixelBlack(pixel):
         if item != 0:
             return False
     return True
+
+if __name__=="__main__":
+    image = [[[255,255,255],[255,255,255],[255,255,255]],
+             [[255,255,255],   [0,0,0],   [255,255,255]],
+             [[255,255,255],[255,255,255],[255,255,255]]]
+    image = cv.imread("map2.png")
+    mm = MeshMaker(image)
+    mm.makeMesh()
     
